527b0beee380fba2872aae536fe930497f9819b9
Phil Salant <PSalant@gmail.com>
2021-12-04 22:23:03 -0500

    Solve day 4, part 2

--- 04/04.go
+++ 04/04.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1())
-}
-
-func part1() int {
 	boards := make([]*Board, len(bingoBoards))
 	for i, board := range bingoBoards {
 		boards[i] = NewBoard(board)
 	}
 
+	fmt.Printf("Part 1: %d\n", part1(boards))
+	// Part 1 modifies `boards`, but because of the
+	// nature of the questions, this does not interfere
+	// with the calculation of part 2's answer.
+	fmt.Printf("Part 2: %d\n", part2(boards))
+}
+
+func part1(boards []*Board) int {
 	for _, number := range drawn {
 		for _, board := range boards {
 			if _, ok := board.Values[number]; ok {
@@ -24,4 +28,27 @@ func part1() int {
 	}
 
 	return 0
+}
+
+func part2(boards []*Board) int {
+	var lastDrawnNumber int
+	var lastWinningBoard *Board
+
+	for _, number := range drawn {
+		for _, board := range boards {
+			if board.Bingo {
+				continue
+			}
+
+			if _, ok := board.Values[number]; ok {
+				board.Mark(number)
+				if board.Bingo {
+					lastWinningBoard = board
+					lastDrawnNumber = number
+				}
+			}
+		}
+	}
+
+	return lastWinningBoard.Score(lastDrawnNumber)
 }

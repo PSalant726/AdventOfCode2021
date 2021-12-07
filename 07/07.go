0db0083fe4b362446d6011802e36a101c6774a49
Phil Salant <PSalant@gmail.com>
2021-12-07 12:06:40 -0500

    Solve day 7, part 2

--- 07/07.go
+++ 07/07.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
 }
 
 func part1() int {
@@ -14,17 +15,46 @@ func part1() int {
 
 	var fuel int
 	for _, position := range input {
-		if position >= idealPosition {
-			fuel += position - idealPosition
-		} else {
-			fuel += idealPosition - position
-		}
+		fuel += difference(position, idealPosition)
 	}
 
 	return fuel
 }
 
+func part2() int {
+	idealPosition := mean(input)
+
+	var fuel int
+	for _, position := range input {
+		fuel += triangularNumber(difference(position, idealPosition))
+	}
+
+	return fuel
+}
+
+func difference(a, b int) int {
+	if a >= b {
+		return a - b
+	}
+
+	return b - a
+}
+
+func mean(slice []int) int {
+	var sum int
+	for _, v := range slice {
+		sum += v
+	}
+
+	return sum / len(slice)
+}
+
 func median(slice []int) int {
 	sort.Ints(slice)
 	return slice[len(slice)/2]
+}
+
+// https://en.wikipedia.org/wiki/Triangular_number
+func triangularNumber(num int) int {
+	return num * (num + 1) / 2
 }

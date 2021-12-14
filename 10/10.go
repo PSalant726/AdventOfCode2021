d899f71d345f3581cb33f9e47f17914637c49389
Phil Salant <PSalant@gmail.com>
2021-12-13 19:26:38 -0500

    Solve day 10, part 2

--- 10/10.go
+++ 10/10.go
@@ -2,54 +2,81 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strings"
 )
 
-var openersToClosers = map[string]string{
-	"(": ")",
-	"[": "]",
-	"{": "}",
-	"<": ">",
-}
-
-var closersToOpeners = map[string]string{
+var openers = map[string]string{
 	")": "(",
 	"]": "[",
 	"}": "{",
 	">": "<",
 }
 
-var closerScores = map[string]int{
-	")": 3,
-	"]": 57,
-	"}": 1197,
-	">": 25137,
+var closers = map[string]string{
+	"(": ")",
+	"[": "]",
+	"{": "}",
+	"<": ">",
 }
 
 func main() {
-	fmt.Printf("Part 1: %d\n", part1())
+	syntaxErrorScore, middleCompletionScore := parseNavSubsystem()
+
+	fmt.Printf("Part 1: %d\n", syntaxErrorScore)
+	fmt.Printf("Part 2: %d\n", middleCompletionScore)
 }
 
-func part1() int {
+func parseNavSubsystem() (int, int) {
+	var completionScores []int
 	var syntaxErrorScore int
+	var syntaxErrorScores = map[string]int{
+		")": 3,
+		"]": 57,
+		"}": 1197,
+		">": 25137,
+	}
 
 	for _, line := range input {
 		var charStack []string
-		chars := strings.Split(line, "")
+		var corrupted bool
 
+		chars := strings.Split(line, "")
 		for _, char := range chars {
-			if _, charIsOpener := openersToClosers[char]; charIsOpener {
+			if _, isOpener := closers[char]; isOpener {
 				charStack = append(charStack, char)
-			} else if score, charIsCloser := closerScores[char]; charIsCloser {
-				if charStack[len(charStack)-1] == closersToOpeners[char] {
-					charStack = charStack[:len(charStack)-1]
-				} else {
-					syntaxErrorScore += score
-					break
-				}
+			} else if charStack[len(charStack)-1] == openers[char] {
+				charStack = charStack[:len(charStack)-1]
+			} else {
+				syntaxErrorScore += syntaxErrorScores[char]
+				corrupted = true
+				break
 			}
+		}
+
+		if !corrupted {
+			completionScores = append(completionScores, completionScore(charStack))
 		}
 	}
 
-	return syntaxErrorScore
+	sort.Ints(completionScores)
+
+	return syntaxErrorScore, completionScores[len(completionScores)/2]
+}
+
+func completionScore(remainingStack []string) int {
+	var completionScore int
+	var scores = map[string]int{
+		")": 1,
+		"]": 2,
+		"}": 3,
+		">": 4,
+	}
+
+	for i := len(remainingStack) - 1; i >= 0; i-- {
+		completionChar := closers[remainingStack[i]]
+		completionScore = completionScore*5 + scores[completionChar]
+	}
+
+	return completionScore
 }
